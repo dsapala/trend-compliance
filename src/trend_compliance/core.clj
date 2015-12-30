f93b77ac99e17142835fe39fe8d21cdea9f59ea4
Dan Sapala <dsapala@gmail.com>
2015-12-30 15:54:59 -0500

    refactor user map

--- src/trend_compliance/core.clj
+++ src/trend_compliance/core.clj
@@ -1,9 +1,17 @@
 (ns trend-compliance.core
   (:require [clojure.java.io :as io]
             [clojure.pprint :as pp]
-            [clojure.string :as str]
+            [clojure.string :as st]
             [clojure-csv.core :as csv])
   (:gen-class))
+
+(def log-map (atom {}))
+(def user-map (atom {}))
+(def blank-line-regex #"^\s*$")
+(def date-regex #"(?i)\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}")
+(def mac-regex #"(?i)Acct-Session-Id\s\=\s\"(.+)\"")
+(def ip-regex #"(?i)Framed-IP-Address\s\=\s(.+)")
+(def username-regex #"(?i)User-Name\s\=\s\"(.+)\"")
 
 (defn create-map
   "parse the list of lists returned from read-csv"
@@ -34,42 +42,68 @@
   [item]
   (filter (every-pred windows? ping-successful? trend-installed?) item))
 
-(def log-map (atom {}))
-(def user-map (atom {}))
+;; (defn create-log-map
+;;   "take a line from detail and put into map using an atom"
+;;   [user-map]
+;;   (swap! log-map assoc :ip (get user-map :ip))
+;;   (swap! log-map assoc :user-name (get user-map :user-name))
+;;   (swap! log-map assoc :mac-ad (get user-map :mac-ad))
+;;   (swap! log-map assoc :date (get user-map :date)))
 
-(defn create-log-map
-  "take a line from detail and put into map using an atom"
-  [user-map]
-  (swap! log-map assoc :ip (get user-map :ip))
-  (swap! log-map assoc :user-name (get user-map :user-name))
-  (swap! log-map assoc :mac-ad (get user-map :mac-ad))
-  (swap! log-map assoc :date (get user-map :date)))
+;; (defn create-user-map
+;;   "parse each line of a large file"
+;;   [line]
+;;   (let [l (str/split line #" = ")]
+;;   (cond
+;;     (re-matches #"(?i)\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}" (str (first l)))
+;;       (swap! user-map assoc :date (str (first l)))
+;;     (re-matches #"(?i)(.*)Acct-Session-Id" (str (first l)))
+;;       (swap! user-map assoc :Acct-Session-Id (re-find #"[^\\\"]+" (str (second l))))
+;;     (re-matches #"(?i)(.*)Framed-IP-Address" (str (first l)))
+;;       (swap! user-map assoc :Framed-IP-Address (re-find #"[^\\\"]+" (str (second l))))
+;;     (re-matches #"(?i)(.*)User-Name" (str (first l)))
+;;       (swap! user-map assoc :User-Name (re-find #"[^\\\"]+" (str (second l)))))))
 
-(defn create-user-map
-  "parse each line of a large file"
-  [line]
-  (let [l (str/split line #" = ")]
-  (cond
-    (re-matches #"(?i)\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}" (str (first l)))
-      (swap! user-map assoc :date (str (first l)))
-    (re-matches #"(?i)(.*)Acct-Session-Id" (str (first l)))
-      (swap! user-map assoc :Acct-Session-Id (re-find #"[^\\\"]+" (str (second l))))
-    (re-matches #"(?i)(.*)Framed-IP-Address" (str (first l)))
-      (swap! user-map assoc :Framed-IP-Address (re-find #"[^\\\"]+" (str (second l))))
-    (re-matches #"(?i)(.*)User-Name" (str (first l)))
-      (swap! user-map assoc :User-Name (re-find #"[^\\\"]+" (str (second l)))))))
+;; (defn read-large-file
+;;   "read in a large detail file"
+;;   [file-name]
+;;   (with-open [rdr (io/reader file-name)]
+;;     (let [line (line-seq rdr)
+;;           pop-user-map (doall (map create-user-map line))
+;;           user-map {:ip (get @user-map :Framed-IP-Address)
+;;                     :user-name (get @user-map :User-Name)
+;;                     :mac-ad (get @user-map :Acct-Session-Id)
+;;                     :date (get @user-map :date)}]
+;;       (prn (print-str pop-user-map))
+;;     (create-log-map user-map))))
 
-(defn read-large-file
-  "read in a large detail file"
-  [file-name]
-  (with-open [rdr (io/reader file-name)]
-    (let [line (line-seq rdr)
-          pop-user-map (doall (map create-user-map line))
-          user-map {:ip (get @user-map :Framed-IP-Address)
-                    :user-name (get @user-map :User-Name)
-                    :mac-ad (get @user-map :Acct-Session-Id)
-                    :date (get @user-map :date)}]
-    (create-log-map user-map))))
+(defn match-line [l]
+  (let [tl (st/trim l)
+        blank (re-matches blank-line-regex tl)
+        date (re-matches date-regex tl)
+        mac (re-matches mac-regex tl)
+        ip (re-matches ip-regex tl)
+        username (re-matches username-regex tl)]
+    (cond
+      (= "" blank) (list :blank)
+      (seq date) (list :date date)
+      (seq mac) (list :mac (second mac))
+      (seq ip) (list :ip (second ip))
+      (seq username) (list :username (second username)))))
+
+(defn create-user-map-from-file [f]
+  (with-open [rdr (io/reader f)]
+    (let [lines (line-seq rdr)]
+      (loop [l (first lines)
+             r (rest lines)
+             u {}]
+        (when l
+          (let [match (match-line l)]
+            (case (first match)
+              :blank (do (swap! user-map assoc (:ip u) u)
+                         (recur (first r) (rest r) {}))
+              :skip (recur (first r) (rest r) u)
+              (recur (first r) (rest r) (merge u (apply hash-map match))))))))))
 
 (defn read-csv
   "read in a csv file"
@@ -87,7 +121,7 @@
   [& args]
   (println "Files (<path/to/WIRED.csv> <path/to/WIRELSS.csv> <path/to/DETAIL>): ")
     (let [in (read-line)
-          file-names (str/split in #" ")
+          file-names (st/split in #" ")
           wired (first file-names)
           wireless (second file-names)
           detail (nth file-names 2)
